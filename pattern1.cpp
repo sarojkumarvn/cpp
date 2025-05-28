87315540f7bb6e7a229311c9eaa9283d209476c3
sarojkumar <sarojojha2457@gmail.com>
2025-05-29 00:00:49 +0530

    Doing pattern

--- pattern1.cpp
+++ pattern1.cpp
@@ -60,17 +60,65 @@ void pattern5()
 void pattern6()
 {
     for (int i = 1; i < 6; i++)
-    { // This line is correct
-        for (int j = 1; j <= 6 - i; j++)  // j = 1 to 6 - i (done)
+    {                                    // This line is correct
+        for (int j = 1; j <= 6 - i; j++) // j = 1 to 6 - i (done)
         {
             cout << j << " ";
         }
         cout << endl;
     }
 }
+void pattern7()
+{
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern8()
+{
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        };
+
+        for (int j = 0; j < 2*n - (2*i + 1); j++)
+        {
+            cout << "*";
+        };
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        };
+
+        cout << endl;
+    }
+}
+
 int main()
 {
-    // pattern1();
-    pattern6();
+
+    pattern8();
     return 0;
 }
