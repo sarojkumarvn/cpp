2b4664bc63da18c97b2fb4f80ba9e056401e7b96
sarojkumar <sarojojha2457@gmail.com>
2025-05-31 14:49:57 +0530

    Done with the patterns

--- pattern1.cpp
+++ pattern1.cpp
@@ -268,9 +268,124 @@ void pattern18() // TO BE CHECKED FOR ISSUE
         cout << endl;
     }
 }
+void pattern19()
+{
+    int n = 10;
+    for (int i = 0; i < n; i++)
+    {
+        // stars
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        // space
+        for (int j = 0; j < 2 * i; j++)
+        {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Second Part
+    for (int i = 1; i <= n; i++)
+    {
+        // stars
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        // space
+        for (int j = 0; j < 2 * n - 2 * i; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void pattern20()
+{
+    int n = 5;
+    int space = 2 * n - 2;
+
+    for (int i = 1; i <= 2 * n - 1; i++) // outer loop
+    {
+        // star
+        int stars = i;
+        if (i > n)
+            stars = 2 * n - i;
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+
+        // space
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        if (i < n)
+            space -= 2;
+        else
+            space += 2;
+    }
+}
+void pattern21()
+{
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || i == n - 1 || j == 0 || j == n - 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+void pattern22()
+{
+    int n = 5;
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2 * n - 2) - j;
+            int down = (2 * n - 2) - i;
+            cout << (n - min(min(top, down), min(left, right)));
+        }
+        cout << endl;
+    }
+}
 int main()
+
 {
 
-    pattern18();
+    pattern22();
+    // cout << "Hello world ";
     return 0;
 }
